f6d9a888582df29efc7144d99cebe8deaa128b6c
Sebastian Bauersfeld <>
2023-03-29 10:05:28 +0700

    fix integration tests on macos

--- integration-tests/iterate-cli-versions.sh
+++ integration-tests/iterate-cli-versions.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 here="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
@@ -12,7 +12,7 @@ for v in $(printf "$topn"); do
   echo "Testing with version ${v}..."
   # introduce random wait time to make sure parallel tests
   # are not all downloading the cli at the same time
-  sleep "$(shuf -i 0-60 -n 1)"
+  sleep "$(($RANDOM % 60))"
   gh codeql set-version "$v"
   "${here}/roundtrip.sh" "$lang"
 done
